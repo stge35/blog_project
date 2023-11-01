956a4bfbaffd77bec9a54e87632d9ca8e0342455
stge35 <evanok1@gmail.com>
2023-11-01 17:05:43 +0900

    fix Content Service Test CRUD

--- server/src/main/java/com/blog_project/server/domain/content/repository/ContentRepository.java
+++ server/src/main/java/com/blog_project/server/domain/content/repository/ContentRepository.java
@@ -3,8 +3,11 @@ package com.blog_project.server.domain.content.repository;
 import com.blog_project.server.domain.content.entity.Content;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface ContentRepository extends JpaRepository<Content, Long> {
     Content findByTitle(String title);
+    List<Content> findByContentId(Long contentId);
 }
 
--- server/src/test/java/com/blog_project/server/content/service/ContentServiceTest.java
+++ server/src/test/java/com/blog_project/server/content/service/ContentServiceTest.java
@@ -11,10 +11,15 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.*;
+
 
 @DataJpaTest
 @Transactional
@@ -44,6 +49,76 @@ public class ContentServiceTest {
         assertThat(result.getTitle()).isEqualTo("content test title");
     }
 
+    @DisplayName("컨텐츠 추가 실패 - 관리자가 아님")
+    @Test
+    public void 컨텐츠_추가_실패_관리자가아님() {
+
+        // given
+
+        // when
+
+        // then
+
+    }
+
+
+    @DisplayName("컨텐츠 검색 성공")
+    @Test
+    public void 컨텐츠_성공() {
+
+        // given
+
+        doReturn(Optional.of(content())).when(contentRepository).findById(content().getContentId());
+
+        // when
+
+        final ContentResponseDto result = target.findContent(content().getContentId());
+
+        // then
+
+        assertThat(result.getTitle()).isEqualTo("content test title");
+        assertThat(result.getBody()).isEqualTo("content test body");
+    }
+
+    @DisplayName("컨텐츠 상세 검색 성공")
+    @Test
+    public void 컨텐츠_상세_검색_성공() {
+
+        // given
+
+        doReturn(Arrays.asList(
+                Content.builder().build(),
+                Content.builder().build(),
+                Content.builder().build())).when(contentRepository).findByContentId(content().getContentId());
+
+        // when
+
+        final List<ContentResponseDto> result = target.findContents(content().getContentId());
+
+        // then
+
+        assertThat(result.size()).isEqualTo(3);
+    }
+
+    @DisplayName("컨텐츠 삭제 성공")
+    @Test
+    public void 컨텐츠_삭제_성공() {
+
+        // given
+
+        final Content content = content();
+
+        doReturn(Optional.of(content)).when(contentRepository).findById(content.getContentId());
+
+        // when
+
+        target.deleteContent(content.getContentId());
+
+        // then(확인 방법을 찾아보자)
+
+
+    }
+
     private Content content() {
         return Content.builder()
                 .title("content test title")
@@ -68,6 +143,38 @@ public class ContentServiceTest {
                     .title(saveContent.getTitle())
                     .body(saveContent.getBody())
                     .build();
+        }
+
+        public ContentResponseDto findContent(final Long contentId) {
+
+            final Optional<Content> optionalContent = contentRepository.findById(contentId);
+            final Content content = optionalContent.orElseThrow(() -> new RuntimeException("존재하지 않습니다."));
+
+            return ContentResponseDto.builder()
+                    .contentId(content.getContentId())
+                    .title(content.getTitle())
+                    .body(content.getBody())
+                    .createdAt(content.getCreatedAt())
+                    .modifiedAt(content.getModifiedAt())
+                    .build();
+        }
+
+        public List<ContentResponseDto> findContents(final Long contentId) {
+            final List<Content> contentList = contentRepository.findByContentId(content().getContentId());
+
+            return contentList.stream().map(content -> ContentResponseDto.builder()
+                        .contentId(content().getContentId())
+                        .title(content().getTitle())
+                        .body(content().getBody())
+                        .createdAt(content().getCreatedAt())
+                        .modifiedAt(content().getModifiedAt())
+                        .build())
+                    .collect(Collectors.toList());
+        }
+
+        public void deleteContent(final Long contentId) {
+
+            contentRepository.deleteById(contentId);
         }
     }
 
