ac1fc772ef4e30dece65732fc48ec9b3d20b516a
stge35 <evanok1@gmail.com>
2023-11-10 17:41:12 +0900

    Add: Configuration CorsConfig

---
+++ server/src/main/java/com/blog_project/server/global/auth/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.blog_project.server.global.auth.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        UrlBasedCorsConfigurationSource source= new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOriginPatterns(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setExposedHeaders(Arrays.asList("*"));
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**,", config);
+
+        return new CorsFilter(source);
+    }
+}
